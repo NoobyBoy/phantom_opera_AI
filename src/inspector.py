d9740b73d84cc8dfa19690948c64cd2905039ca2
dguillaume <guillaume.dussourd@epitech.eu>
2021-10-10 20:27:11 +0200

    IA inspector

--- src/inspector.py
+++ src/inspector.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 
+"""
+    The inspector class
+
+
+"""
 
 import json
 import logging
@@ -39,6 +44,12 @@ inspector_logger.addHandler(stream_handler)
 
 class Inspector(BasePlayer):
 
+    charPriority = ["grey", "brown", "purple", "white", "pink", "black", "blue"]
+    charCanScream = []
+    NoPowerRoom = 0
+    playerPowerRoom = 0
+    splitChar = False #true if we want to split them, false if we want them together
+
     def __init__(self):
         BasePlayer.__init__(self)
         self.end = False
@@ -59,17 +70,23 @@ class Inspector(BasePlayer):
         inspector_logger.debug(f"response index ---- {self.response_index}")
         inspector_logger.debug(f"response ---------- {self.possible_answer[self.response_index]}")
 
-    def getInfoCharacter(self):
-        print("All infos for all cards")
-        for i in self.possible_answer:
-            print(i)
-            L = self.getPossibleMovement(i)
-            print(L)
-            print("room status")
-            for j in L:
-                print(self.getRoomStatus(j))
+    def CanScream(self):#fill a list with all characters who can potentially scream
+        """
+            Check how many people can scream and list them, the goal is to have at least 2 people who can scream
+            A character can scream if he's suspect, and alone or in the room without power
+            The goal is to know if we have enough suspect who can scream to assemble people or if we need to split them
+        """
+        self.charCanScream.clear()
+        for charact in self.game_state["characters"]:
+            if charact["suspect"] == True:
+                if self.getCharacterNbInRoom(charact["position"]) == 1 or charact["position"] == self.game_state["shadow"]:
+                    self.charCanScream.append(charact)
 
-    def getNbInRoom(self):
+    def getNbInRoom(self): 
+        """
+            Count if there are some group or not to choose a strategy (Split or Together functions) and return the number with the more people in it
+            retourne le numéro de salle avec le plus de personne
+        """
         nb = 0
         nbRoom = 0
         for i in range(9):
@@ -79,7 +96,10 @@ class Inspector(BasePlayer):
                 nbRoom = i
         return nbRoom
 
-    def splitCharacters(self):
+    def splitCharacters(self):#split the selected character and answer directly
+        """
+            Count the number of characters in the possibles answers and return the room with the less character in it
+        """
         print("split characters")
         nb = 8
         indexToReturn = 0
@@ -91,7 +111,10 @@ class Inspector(BasePlayer):
         self.response_index = indexToReturn
 
 
-    def togetherCharacters(self):
+    def togetherCharacters(self): ## Bring characters together and answer directly
+        """
+            Count the number of characters in the possibles answers and return the room with the more character in it
+        """
         print("Bring characters together")
         nb = 0
         indexToReturn = 0
@@ -99,58 +122,141 @@ class Inspector(BasePlayer):
             if self.getCharacterNbInRoom(i) > nb:
                 indexToReturn = self.possible_answer.index(i)
                 nb = self.getCharacterNbInRoom(i)
-        print("index to return = ", indexToReturn)
+        print("index to return = ", indexToReturn)            
+        print(self.game_state["shadow"])
         self.response_index = indexToReturn
 
+    def ChooseStrategy(self): # function where we decide which strategy we will use
+        """
+            3 cas (choisir en priorité un suspect): 
+            il est suspect et peut crier (si assez de cas comme lui, le regrouper, sinon tout seul), 
+            il est suspect mais ne peut pas crier (si assez de sus qui peuvent crier, l'empêcher de crier, sinon le faire crier) 
+            il est innocent (il y a assez de sus qui crient, random sinon en fonction de la couleur, faire en sorte qu'il puisse crier) done
+        """
+        screamPlayable = []
+        susplayable = self.getSuspectPlayable()
+        print("List of Playable characters ", self.game_state["active character_cards"])
+        
+        if susplayable == 0: # if there is no sus in the choice of character
+            if len(self.charCanScream) > 2: # if there is more than 2 char who can scream, we try to reduce this number
+                self.splitChar = False
+                self.returnCharacter(self.getInnocentPlayable())
+            else:                           # else we split them to create more char who can scream
+                self.splitChar = True
+                self.returnCharacter(self.getInnocentPlayable())
+
+        for char in susplayable:
+            if char in self.charCanScream:
+                screamPlayable.append(char)
+        
+        if len(self.charCanScream) > 2 and len(screamPlayable) != 0: # If there is more than 2 char who can scream, we want to reduce the number to 2 so we try to create a group
+            self.splitChar = False
+            self.returnCharacter(screamPlayable) # choose a character in the list of char who can scream according to the priority list
+        
+        if len(self.charCanScream) < 2 and len(susplayable) != 0: # if there is not enough char who can scream, we try to split them
+            self.splitChar = True
+            self.returnCharacter(susplayable) #choose a character in the list of suspect char according to the priority list
+
+
+    def returnCharacter(self, charlist):
+        for char in charlist:
+            for priority in self.charPriority:
+                if priority == char["color"]:
+                    self.response_index = self.getIndexOfColor(priority)
+
+
+    def CharactCanScream(self, charact):#return true or false if the character can scream or not
+        """
+            check if a given character can scream
+        """
+        if charact in self.charCanScream:
+            return True
+        else:
+            return False 
     
-    def makeStrategyChoice(self): ## get infos for all rooms to know if inspector separate people or to assemble them
-        if self.getCharacterNbInRoom(self.getNbInRoom()) > 3:
+    def makeStrategyChoice(self): 
+        """ 
+            get infos for all rooms to know if inspector separate people or assemble them
+            If there is a group of 3 or more, we split people
+            If there is no group of 3 or more people, we assemble them
+        """
+        if self.splitChar:
             self.splitCharacters()
         else:
             self.togetherCharacters()
-        print("Possible answer are:")
-        print(self.possible_answer)
         
 
     def selectCharacter(self):
+        self.ChooseStrategy()
         if ("red" in self.getActiveCardsColors()):
             self.response_index = self.getIndexOfColor("red")
+
+
         else:
             self.response_index = randint(0, len(self.possible_answer) - 1)
 
     def selectPosition(self):
         print(self.response_index)
         self.makeStrategyChoice()
-        ##self.response_index = 0
 
-    def selectActavationOfpower(self):
+    def selectActivationOfpower(self):
+        print(self.possible_answer)
         self.response_index = 0
 
     def selectPurplePower(self):
         """
-            Pruple: Can swap position with another character
+            Purple: Can swap position with another character 
+
+            If purple innocent => change position with suspect who cannot scream if there is not enough suspect who can scream
+            else, change position with character who can scream so he can't scream anymore
+
+            if purple sus => if there is enough suspect who can scream, go where purple cannot scream
+            else, change with innocent alone
         """
-        self.response_index = 0
+        if self.getCharacterByColor("purple")["suspect"]:
+            if len(self.charCanScream) > 2:
+                print("Do something")
+        else:
+            print("purple is inno")
+        #self.response_index = 0
 
     def selectBrownPower(self):
         """
-            Brown: Move other characters with him
+            Brown: Move other characters with him => if 1 or 2 suspect with him, move, if possible, in the room with no light
         """
         self.response_index = 0
 
 
-    def selectGreyPower(self):
+    def selectGreyPower(self): ## fonction dans laquelle on choisi la pièce où mettre la panne de courant
         """
-            Grey: Move the 'Electrical problem' token
+            Grey: Move the 'Electrical problem' token 
+            => check if there is a group and how many suspect are inside, if there is 1 or 2 sus, move the 'problem' in the room
         """
-        self.response_index = 0
+        print("Select Grey Power", self.possible_answer)
+        answer = -1
+        for i in range(len(self.possible_answer)-1):
+            print(i)
+            a = self.getCharacterNbInRoom(i)
+            if a > 1:
+                nb = self.getSuspecNbInRoom(i)
+                if nb == 1:
+                    answer = i
+        if answer == -1:
+            answer = randint(0, len(self.possible_answer) - 1)
+        self.response_index = answer
 
 
-    def selectBluePowerRoom(self):
+    def selectBluePowerRoom(self): ## fonction dans laquelle on choisi la pièce où mettre le lock
         """
             Blue: Move the 'lock' token (room)
+            Select a room without anybody
         """
-        self.response_index = 0
+        answer = 0
+        for i in self.possible_answer:
+            if self.getCharacterNbInRoom(i) == 0:
+                answer = i
+
+        self.response_index = answer
 
 
     def selectBluePowerExit(self):
@@ -160,26 +266,30 @@ class Inspector(BasePlayer):
         self.response_index = 0
 
 
-    def selectWhitePower(self):
+    def selectWhitePower(self): ## check si combien de suspects/innocents composent un groupe, essayer de regrouper les innocents et de split les suspects s'il y en a moins de 2
         """
-            White: Move the other characters from the room
+            White: Move the other characters from the room => use the power if the strategy is to split and if there is 1 suspect in the group
         """
         self.response_index = 0
 
 
     def answer(self):
         if (self.question == "select character"):
+            NoPowerRoom = self.game_state["shadow"]
+            self.CanScream()
+            print("list of character who can scream ", self.charCanScream)
             self.selectCharacter()
         if (self.question == "select position"):
             self.selectPosition()
         if ("activate" in self.question and "power" in self.question):
-            self.selectActavationOfpower()
+            self.selectActivationOfpower()
         if (self.question == "purple character power"):
             self.selectPurplePower()
         if (self.question == "brown character power"):
             self.selectBrownPower()
         if (self.question == "grey character power"):
             self.selectGreyPower()
+            #import pdb; pdb.set_trace()
         if (self.question == "blue character power room"):
             self.selectBluePowerRoom()
         if (self.question == "blue character power exit"):
